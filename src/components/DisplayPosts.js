224a2dd051b15dd7fbcc6e14326387a7345b2e61
jpca999 <jpca999@gmail.com>
2020-10-17 18:25:59 -0700

    after updating the api key and url in the aws-exports its showing the JSON result in the console

--- src/components/DisplayPosts.js
+++ src/components/DisplayPosts.js
@@ -1,6 +1,27 @@
 import React, {Component} from 'react'
+import { listPosts } from '../graphql/queries'
+import { API, graphqlOperation } from 'aws-amplify'
 
 export default class DisplayPosts extends Component{
+
+    componentDidMount = async() =>{
+        this.getPosts()
+    }
+
+    getPosts = async () => {
+        const result = await API.graphql (graphqlOperation(listPosts))
+        console.log(' results =', JSON.stringify(result.data.listPosts.items)) 
+    };
+
+     response = {
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Credentials" : true,
+        },
+        body: "...",
+        statusCode: 200
+    }
+
     render(){
         return (
             <div> Display Posts </div>
